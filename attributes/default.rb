d26c4c0428fbdb2b068fc16173435c3d0fb11016
Richard Shade <rshade@rightscale.com>
2013-12-05 16:07:45 -0600

    adding full path for binaries

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,8 @@
 case node[:platform]
 when "ubuntu","debian"
   default[:google_cloud][:python][:pkg]="python"
-  default[:google_cloud][:python][:pkg]="python"
+  default[:google_cloud][:python][:bin]="/usr/bin/python"
 when "redhat","centos"
   default[:google_cloud][:python][:pkg]="python27"
-  default[:google_cloud][:python][:bin]="python2.7"
+  default[:google_cloud][:python][:bin]="/usr/bin/python2.7"
 end
